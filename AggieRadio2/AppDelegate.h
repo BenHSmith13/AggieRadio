2b00f8442a7bb6e5f2e3c146d3d5d5d58d551731
bensmith <benhsmith13@gmail.com>
2015-04-14 15:43:47 -0600

    Initial Commit

---
+++ AggieRadio2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AggieRadio2
+//
+//  Created by Ben Smith on 4/14/15.
+//  Copyright (c) 2015 Ben Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AggieRadio2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AggieRadio2
+//
+//  Created by Ben Smith on 4/14/15.
+//  Copyright (c) 2015 Ben Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
